9eb503bc2826008953332827b0bbf7ff08fc3dbc
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-08-28 23:15:46 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://rocketnotes-api.sergiomello.online",
+    baseURL: "https://rocketmovies-api.sergiomello.online",
 })
