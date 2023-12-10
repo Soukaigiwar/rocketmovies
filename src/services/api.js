ed786ef90d989e5b94de1e429c6726da2837ef14
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-12-10 21:27:13 +0000

    using render.com

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,6 @@
 import axios from "axios"
 
 export const api = axios.create({
-<<<<<<< HEAD
-    baseURL: "http://localhost:4444",
-    //baseURL: "https://rocketmovies-api.sergiomello.online",
-=======
+    //baseURL: "http://localhost:4444",
     baseURL: "https://rocketmovies-api.sergiomello.online",
->>>>>>> 9eb503bc2826008953332827b0bbf7ff08fc3dbc
 })
