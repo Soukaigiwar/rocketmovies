bf359d95e3c8adf6f8c571406ad4cc479242bc72
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-12-10 22:53:05 +0000

    change base_url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL || "http://localhost:3334"
+    baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3334"
 });
